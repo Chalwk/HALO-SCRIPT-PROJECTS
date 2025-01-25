f691c7eb4acc3e6b7aa70768eb1f6e51d0689645
Chalwk77 <jericho.crosby227@gmail.com>
2025-01-25 14:19:46 +1300

    improvements

--- SAPP SCRIPTS/UTILITY MODS/Get Object Identity.lua
+++ SAPP SCRIPTS/UTILITY MODS/Get Object Identity.lua
@@ -1,133 +1,153 @@
 --[[
---=====================================================================================================--
+=====================================================================================================
 Script Name: Get Object Identity, for SAPP (PC & CE)
 
-Command Syntax:
-/getidentity on|off
+Command: /get_objects
 
 Point your crosshair at any object and fire your weapon.
-The mod will display the following information in the rcon console:
+The script will display the following information:
 
-Object Type
-Object Name
-Object Meta ID
-Object X,Y,Z coordinates
+    - Object Type
+    - Object Name
+    - Object Meta ID
+    - Object X, Y, Z coordinates
 
-Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
+    For example:
+        Type: vehicle
+        Name: vehicles\warthog\mp_warthog
+        Meta: 0
+        X, Y, Z: 0.000000, 0.000000, 0.000000
+
+Copyright (c) 2016-2025, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this script subject to the following conditions:
 https://github.com/Chalwk/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
---=====================================================================================================--
+=====================================================================================================
 ]]
 
 api_version = '1.12.0.0'
 
--- Custom command used to toggle get-mode on/off:
-local command = 'get_objects'
+-----------------------------------------
+-- Config starts here, edit as needed --
+-----------------------------------------
 
--- Minimum permission level required to execute the custom command:
-local permission_level = 4
+-- Command to toggle get-object mode:
+local COMMAND = 'get_objects'
+-- Minimum permission level required to use the command:
+local PERMISSION_LEVEL = 4
 
-local admins = {}
+-----------------------------------------
+-- Config ends here, do not edit below --
+-----------------------------------------
+
+-- Table to store active admin sessions:
+local active_admins = {}
 
 function OnScriptLoad()
     register_callback(cb['EVENT_TICK'], "OnTick")
     register_callback(cb['EVENT_COMMAND'], "OnCommand")
-    register_callback(cb['EVENT_GAME_START'], "OnStart")
-    OnStart()
+    register_callback(cb['EVENT_GAME_START'], "OnGameStart")
+    OnGameStart()
 end
 
-function OnStart()
-    if (get_var(0, '$gt') ~= 'n/a') then
-        admins = {}
+function OnGameStart()
+    if get_var(0, '$gt') ~= 'n/a' then
+        active_admins = {}
     end
 end
 
-local function GetCamXYZ(dyn)
+--- Helper function to get player camera and position vectors ---
+local function GetCameraAndPosition(dyn_player)
+    local cam_x = read_float(dyn_player + 0x230)
+    local cam_y = read_float(dyn_player + 0x234)
+    local cam_z = read_float(dyn_player + 0x238)
+    local couching = read_float(dyn_player + 0x50C)
+    local pos_x, pos_y, pos_z = read_vector3d(dyn_player + 0x5C)
 
-    local cam_x = read_float(dyn + 0x230)
-    local cam_y = read_float(dyn + 0x234)
-    local cam_z = read_float(dyn + 0x238)
-    local couching = read_float(dyn + 0x50C)
-    local cx, cy, cz = read_vector3d(dyn + 0x5c)
-
-    if (couching == 0) then
-        cz = cz + 0.65
-    else
-        cz = cz + (0.35 * couching)
-    end
-
-    return cx, cy, cz, cam_x * 1000, cam_y * 1000, cam_z * 1000
+    -- Adjust Z-coordinate for crouching:
+    pos_z = pos_z + (couching == 0 and 0.65 or 0.35 * couching)
+    return pos_x, pos_y, pos_z, cam_x * 1000, cam_y * 1000, cam_z * 1000
 end
 
-local function Tell(Ply, Type, Name, Meta, ox, oy, oz)
-
-    -- clear player rcon: (writes 25 blank lines to it)
+--- Helper function to display object information ---
+local function DisplayObjectInfo(player, object_type, object_name, meta_id, x, y, z)
+    -- Clear player RCON:
     for _ = 1, 25 do
-        rprint(Ply, " ")
+        rprint(player, " ")
     end
 
-    -- print to player rcon:
-    rprint(Ply, 'Type: ' .. Type)
-    rprint(Ply, 'Name: ' .. Name)
-    rprint(Ply, 'Meta: ' .. Meta)
-    rprint(Ply, 'X,Y,Z: ' .. ox .. ', ' .. oy .. ', ' .. oz)
+    -- Send details to player's RCON:
+    rprint(player, 'Type: ' .. object_type)
+    rprint(player, 'Name: ' .. object_name)
+    rprint(player, 'Meta: ' .. meta_id)
+    rprint(player, 'X, Y, Z: ' .. x .. ', ' .. y .. ', ' .. z)
 
-    -- print to terminal:
-    cprint('Type: ' .. Type)
-    cprint('Name: ' .. Name)
-    cprint('Meta: ' .. Meta)
-    cprint('X,Y,Z: ' .. ox .. ', ' .. oy .. ', ' .. oz)
+    -- Log details to server console:
+    cprint('Type: ' .. object_type)
+    cprint('Name: ' .. object_name)
+    cprint('Meta: ' .. meta_id)
+    cprint('X, Y, Z: ' .. x .. ', ' .. y .. ', ' .. z)
 end
 
 function OnTick()
-    for i, v in pairs(admins) do
+    for player, data in pairs(active_admins) do
+        local dyn_player = get_dynamic_player(player)
+        if dyn_player ~= 0 and player_alive(player) then
 
-        local dyn = get_dynamic_player(i)
-        if (dyn ~= 0 and player_alive(i)) then
+            local px, py, pz, cx, cy, cz = GetCameraAndPosition(dyn_player)
+            local ignore_player = read_dword(get_player(player) + 0x34)
 
-            local px, py, pz, cx, cy, cz = GetCamXYZ(dyn)
-            local ignore_player = read_dword(get_player(i) + 0x34)
-            local success, _, _, _, target = intersect(px, py, pz, cx * 1000, cy * 1000, cz * 1000, ignore_player)
-
-            if (success and target) then
-
-                local shooting = read_float(dyn + 0x490)
-                if (shooting ~= v.click and shooting == 1) then
+            local success, _, _, _, target = intersect(px, py, pz, cx, cy, cz, ignore_player)
+            if success and target then
+                local is_shooting = read_float(dyn_player + 0x490)
+                if is_shooting ~= data.last_shoot and is_shooting == 1 then
                     local object = get_object_memory(target)
-                    if (object ~= 0) then
+                    if object ~= 0 then
 
-                        local type = read_byte(object + 0xB4)
-                        local name = read_string(read_dword(read_word(object) * 32 + 0x40440038))
-                        local meta = read_dword(object)
+                        local obj_type = read_byte(object + 0xB4)
+                        local obj_name = read_string(read_dword(read_word(object) * 32 + 0x40440038))
+                        local meta_id = read_dword(object)
                         local ox, oy, oz = read_vector3d(object + 0x5C)
 
-                        type = (type == 0 and 'bipd') or (type == 1 and 'vehi') or (type == 2 and 'eqip') or 'weap'
-                        Tell(i, type, name, meta, ox, oy, oz)
+                        local type_string = ({ [0] = 'bipd', [1] = 'vehi', [2] = 'eqip', [3] = 'weap' })[obj_type] or 'unknown'
+                        DisplayObjectInfo(player, type_string, obj_name, meta_id, ox, oy, oz)
                     end
                 end
-
-                v.click = shooting
+                data.last_shoot = is_shooting
             end
         end
     end
 end
 
-function OnCommand(Ply, CMD)
-    local lvl = tonumber(get_var(Ply, '$lvl'))
-    if (CMD:sub(1, #command):lower() == command) then
-        if (lvl < permission_level) then
-            rprint(Ply, 'You do not have permission to execute that command')
-        elseif (not admins[Ply]) then
-            admins[Ply] = { click = 0 }
-            rprint(Ply, 'Get Object mode enabled')
-        else
-            admins[Ply] = nil
-            rprint(Ply, 'Get Object mode disabled')
+local function hasPermission(playerId)
+
+    local level = tonumber(get_var(playerId, '$lvl'))
+    if level >= PERMISSION_LEVEL then
+        return true
+    end
+
+    rprint(playerId, 'You do not have permission to execute that command.')
+    return false
+end
+
+function OnCommand(player, cmd)
+    if cmd:lower():sub(1, #COMMAND) == COMMAND then
+
+        if not hasPermission(player) then
+            return false
         end
+
+        if active_admins[player] then
+            active_admins[player] = nil
+            rprint(player, 'Get Object mode disabled.')
+        else
+            active_admins[player] = { last_shoot = 0 }
+            rprint(player, 'Get Object mode enabled.')
+        end
+
         return false
     end
 end
 
 function OnScriptUnload()
-    -- N/A
+    -- Cleanup not required
 end
