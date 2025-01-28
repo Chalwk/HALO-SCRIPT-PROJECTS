057b013cdf3eb66dd56c48ad9c1d849999beb4ce
Chalwk77 <jericho.crosby227@gmail.com>
2025-01-28 14:46:47 +1300

    ...

--- SAPP SCRIPTS/CUSTOM GAMES/Divide & Conquer.lua
+++ SAPP SCRIPTS/CUSTOM GAMES/Divide & Conquer.lua
@@ -5,7 +5,7 @@ Description: When you kill someone on the opposing team, the victim is switched 
              The main objective is to dominate the opposing team.
              When the opposing team has no players left the game is over.
 
-Copyright (c) 2023-2024, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2023-2025, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
