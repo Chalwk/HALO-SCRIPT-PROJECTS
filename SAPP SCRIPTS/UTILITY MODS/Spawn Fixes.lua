e875b6225b1468bb1a455b084dbadbdc17693f55
Chalwk77 <jericho.crosby227@gmail.com>
2025-01-29 19:56:20 +1300

    ...

--- SAPP SCRIPTS/UTILITY MODS/Spawn Fixes.lua
+++ SAPP SCRIPTS/UTILITY MODS/Spawn Fixes.lua
@@ -15,6 +15,8 @@ Description:    The maps listed below each have one or more broken spawn points.
     sniperbluff
     rev_savior_cave_prerelease
 
+    TODO: REFACTOR THIS ASAP
+
 Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this script subject to the following conditions:
 https://github.com/Chalwk/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
