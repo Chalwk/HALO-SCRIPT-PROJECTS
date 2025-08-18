48210aa466679c0edfd78cec36fd1ebb65fb1b61
Chalwk77 <jericho.crosby227@gmail.com>
2025-08-19 10:15:56 +1200

    ...

--- indev/Battle Royale (abandoned)/events/on_drop.lua
+++
@@ -1,9 +0,0 @@
-local event = {}
-
-function event:on_drop(id)
-
-end
-
-register_callback(cb['EVENT_WEAPON_DROP'], 'on_drop')
-
-return event
--- indev/Battle Royale (abandoned)/events/on_pickup.lua
+++
@@ -1,9 +0,0 @@
-local event = {}
-
-function event:on_pickup(id)
-
-end
-
-register_callback(cb['EVENT_WEAPON_PICKUP'], 'on_pickup')
-
-return event
