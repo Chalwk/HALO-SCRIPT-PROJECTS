69802d90ed88d9f6c842581acfb64eed175481fc
Chalwk77 <jericho.crosby227@gmail.com>
2025-08-22 10:10:32 +1200

    ...

--- sapp/attractive/vanish.lua
+++ sapp/attractive/vanish.lua
@@ -90,17 +90,17 @@ end
 local function get_player_position(dyn_player)
     local crouch = read_float(dyn_player + 0x50C)
     local vehicle_id = read_dword(dyn_player + 0x11C)
+    local vehicle_obj = get_object_memory(vehicle_id)
 
     local x, y, z
+    local z_offset = (crouch == 0) and 0.65 or 0.35 * crouch
 
     if vehicle_id == 0xFFFFFFFF then
         x, y, z = read_vector3d(dyn_player + 0x5C)
-    else
-        local vehicle_obj = get_object_memory(vehicle_id)
+    elseif vehicle_obj ~= 0 then
         x, y, z = read_vector3d(vehicle_obj + 0x5C)
     end
 
-    local z_offset = (crouch == 0) and 0.65 or 0.35 * crouch
     return x, y, z + z_offset
 end
 
