9d4c9404fc24c2e138fdd4af5cb57bd8183751e6
Chalwk77 <jericho.crosby227@gmail.com>
2025-08-22 14:19:17 +1200

    ...

--- sapp/utility/map_vote_system.lua
+++ sapp/utility/map_vote_system.lua
@@ -25,14 +25,13 @@ LICENSE:          MIT License
 =====================================================================================
 ]]
 
-api_version = "1.12.0.0"
-
+-- Config Start --------------------------------------------------
 local MapVoteConfig = {
     map_skip_percentage = 60, -- Percentage required for map skip (0 to disable).
-    re_vote_allowed = true, -- Allow players to recast votes.
-    max_re_votes = 1, -- Maximum re-votes allowed per player.
+    re_vote_allowed = true,   -- Allow players to recast votes.
+    max_re_votes = 1,         -- Maximum re-votes allowed per player.
     display_vote_options = 8, -- Number of maps to display for voting.
-    clear_console = false, -- Clear player's console when displaying votes.
+    clear_console = false,    -- Clear player's console when displaying votes.
     message_format = {
         vote_message = "$name voted for [#$id] $map",
         re_vote_message = "$name changed vote to [#$id] $map",
@@ -44,95 +43,58 @@ local MapVoteConfig = {
         vote_limit_reached = "You have reached the re-vote limit.",
     },
     timers = {
-        time_to_show_votes = 7, -- Time to show votes after game ends.
+        time_to_show_votes = 7,   -- Time to show votes after game ends.
         time_to_tally_votes = 13, -- Time to tally votes after voting period ends.
-        time_to_cycle_map = 13, -- Time to cycle map after PGCR.
-        re_show_interval = 3, -- Time interval to re-show vote options.
+        time_to_cycle_map = 13,   -- Time to cycle map after PGCR.
+        re_show_interval = 3,     -- Time interval to re-show vote options.
     },
-    map_repeats_limit = 2, -- Maximum number of times a map can be played consecutively.
+    map_repeats_limit = 2,        -- Maximum number of times a map can be played consecutively.
     map_list = {
-        { "bloodgulch", "ctf" },
-        { "deathisland", "ctf" },
-        { "sidewinder", "ctf" },
-        { "icefields", "ctf" },
-        { "infinity", "ctf" },
-        { "timberland", "ctf" },
-        { "dangercanyon", "ctf" },
-        { "beavercreek", "ctf" },
+        { "bloodgulch",     "ctf" },
+        { "deathisland",    "ctf" },
+        { "sidewinder",     "ctf" },
+        { "icefields",      "ctf" },
+        { "infinity",       "ctf" },
+        { "timberland",     "ctf" },
+        { "dangercanyon",   "ctf" },
+        { "beavercreek",    "ctf" },
         { "boardingaction", "ctf" },
-        { "carousel", "ctf" },
-        { "chillout", "ctf" },
-        { "damnation", "ctf" },
-        { "gephyrophobia", "ctf" },
-        { "hangemhigh", "ctf" },
-        { "longest", "ctf" },
-        { "prisoner", "ctf" },
-        { "putput", "ctf" },
-        { "ratrace", "ctf" },
-        { "wizard", "ctf" }
+        { "carousel",       "ctf" },
+        { "chillout",       "ctf" },
+        { "damnation",      "ctf" },
+        { "gephyrophobia",  "ctf" },
+        { "hangemhigh",     "ctf" },
+        { "longest",        "ctf" },
+        { "prisoner",       "ctf" },
+        { "putput",         "ctf" },
+        { "ratrace",        "ctf" },
+        { "wizard",         "ctf" }
     },
 }
 
--- Script Variables:
+-- Config End --------------------------------------------------
+
+api_version = "1.12.0.0"
+
 local player_votes = {}
 local map_results = {}
-local vote_timer = 0
 local vote_active = false
 local map_streak = { last_map = -1, streak_count = 0 }
 
--- Register events and callbacks.
-function OnScriptLoad()
-    register_callback(cb["EVENT_CHAT"], "OnPlayerVote")
-    register_callback(cb["EVENT_GAME_END"], "OnGameEnd")
-    register_callback(cb["EVENT_GAME_START"], "OnGameStart")
-    register_callback(cb["EVENT_JOIN"], "OnPlayerJoin")
-    register_callback(cb["EVENT_LEAVE"], "OnPlayerLeave")
-    OnGameStart(true)
-end
+local table_insert = table.insert
+local pairs, ipairs, tonumber = pairs, ipairs, tonumber
 
-function OnScriptUnload()
-    -- N/A
-end
+local rprint = rprint
+local player_present, get_var, say_all, timer = player_present, get_var, say_all, timer
 
-local function InitializePlayers()
-    for i = 1, 16 do
-        if player_present(i) then
-            player_votes[i] = MapVoteConfig.max_re_votes
-        end
-    end
-end
-
-local function ResetVoteData()
-    vote_timer = 0
-    vote_active = false
-    map_results = {}
-end
-
-function OnGameStart(reset)
-    if reset then
-        ResetVoteData()
-    end
-    InitializePlayers()
-end
-
-function OnGameEnd()
-    vote_active = true
-    vote_timer = 0
-    timer(25, "DisplayVoteOptions")
-end
-
-local function ClearConsole(Player)
-    for _ = 1, 25 do
-        rprint(Player, " ")
-    end
+local function ClearConsole(id)
+    for _ = 1, 25 do rprint(id, " ") end
 end
 
 local function BroadcastVoteOptions(map_options)
     for i = 1, 16 do
         if player_present(i) then
-            if MapVoteConfig.clear_console then
-                ClearConsole(i)
-            end
+            if MapVoteConfig.clear_console then ClearConsole(i) end
             for j, map in ipairs(map_options) do
                 local message = MapVoteConfig.message_format.map_option_message
                 message = message:gsub("$id", j):gsub("$map", map[1])
@@ -140,6 +102,15 @@ local function BroadcastVoteOptions(map_options)
             end
         end
     end
+end
+
+local function contains(table, element)
+    for _, value in pairs(table) do
+        if value[1] == element[1] then
+            return true
+        end
+    end
+    return false
 end
 
 local function GenerateMapOptions()
@@ -150,7 +121,7 @@ local function GenerateMapOptions()
     for _, map in ipairs(maps) do
         -- Exclude maps that have exceeded the repeat limit:
         if map_streak.last_map ~= map[1] or map_streak.streak_count < MapVoteConfig.map_repeats_limit then
-            table.insert(vote_options, map)
+            table_insert(vote_options, map)
             count = count + 1
         end
         if count >= MapVoteConfig.display_vote_options then
@@ -161,8 +132,8 @@ local function GenerateMapOptions()
     -- If not enough maps were found, allow maps that were excluded due to streak:
     if count < MapVoteConfig.display_vote_options then
         for _, map in ipairs(maps) do
-            if not table.contains(vote_options, map) then
-                table.insert(vote_options, map)
+            if not contains(vote_options, map) then
+                table_insert(vote_options, map)
                 count = count + 1
             end
             if count >= MapVoteConfig.display_vote_options then
@@ -174,44 +145,40 @@ local function GenerateMapOptions()
     return vote_options
 end
 
+local function ProcessPlayerVote(id, vote_id)
+    if player_votes[id] > 0 then
+        player_votes[id] = player_votes[id] - 1
+        local map_voted = MapVoteConfig.map_list[vote_id][1]
+        local vote_message = MapVoteConfig.message_format.vote_message
+        vote_message = vote_message:gsub("$name", get_var(id, "$name")):gsub("$id", vote_id):gsub("$map", map_voted)
+        say_all(vote_message)
+
+        -- Tally the votes:
+        map_results[vote_id] = (map_results[vote_id] or 0) + 1
+    else
+        rprint(id, MapVoteConfig.message_format.vote_limit_reached)
+    end
+end
+
 function DisplayVoteOptions()
     local map_options = GenerateMapOptions()
     BroadcastVoteOptions(map_options)
     timer(MapVoteConfig.timers.time_to_tally_votes * 1000, "TallyVotesAndSelectMap") -- Call to tally votes after a delay
 end
 
-function OnPlayerJoin(player)
-    player_votes[player] = MapVoteConfig.max_re_votes
-end
-
-function OnPlayerLeave(player)
-    player_votes[player] = nil
-end
-
-local function ProcessPlayerVote(Player, vote_id)
-    if player_votes[Player] > 0 then
-        player_votes[Player] = player_votes[Player] - 1
-        local map_voted = MapVoteConfig.map_list[vote_id][1]
-        local vote_message = MapVoteConfig.message_format.vote_message
-        vote_message = vote_message:gsub("$name", get_var(Player, "$name")):gsub("$id", vote_id):gsub("$map", map_voted)
-        say_all(vote_message)
-
-        -- Tally the votes:
-        map_results[vote_id] = (map_results[vote_id] or 0) + 1
-    else
-        rprint(Player, MapVoteConfig.message_format.vote_limit_reached)
-    end
-end
-
-function OnPlayerVote(Player, Message)
+function OnPlayerVote(id, msg)
     if vote_active then
-        local vote_id = tonumber(Message)
+        local vote_id = tonumber(msg)
         if vote_id and vote_id >= 1 and vote_id <= #MapVoteConfig.map_list then
-            ProcessPlayerVote(Player, vote_id)
+            ProcessPlayerVote(id, vote_id)
         else
-            rprint(Player, MapVoteConfig.message_format.invalid_vote:gsub("$max", #MapVoteConfig.map_list))
+            rprint(id, MapVoteConfig.message_format.invalid_vote:gsub("$max", #MapVoteConfig.map_list))
         end
     end
+end
+
+function LoadMap(map, mode)
+    execute_command('map ' .. map .. ' "' .. mode .. '"')
 end
 
 function TallyVotesAndSelectMap()
@@ -242,21 +209,41 @@ function TallyVotesAndSelectMap()
     end
 
     -- Cycle to the selected map:
-    timer(MapVoteConfig.timers.time_to_cycle_map * 1000, "load_map", winning_map[1], winning_map[2])
+    timer(MapVoteConfig.timers.time_to_cycle_map * 1000, "LoadMap", winning_map[1], winning_map[2])
 end
 
-function OnCommand(PlayerIndex, Command, _, _)
-    -- Custom commands if needed.
-end
+function OnStart()
+    if get_var(0, '$gt') == 'n/a' then return end
+    vote_active = false
+    map_results = {}
 
--- Helper function to check if a map is already in the vote options:
-function table.contains(table, element)
-    for _, value in pairs(table) do
-        if value[1] == element[1] then
-            return true
+    for i = 1, 16 do
+        if player_present(i) then
+            player_votes[i] = MapVoteConfig.max_re_votes
         end
     end
-    return false
 end
 
-return false
+function OnEnd()
+    vote_active = true
+    timer(1000, "DisplayVoteOptions")
+end
+
+function OnJoin(id)
+    player_votes[id] = MapVoteConfig.max_re_votes
+end
+
+function OnQuit(id)
+    player_votes[id] = nil
+end
+
+function OnScriptLoad()
+    register_callback(cb["EVENT_JOIN"], "OnJoin")
+    register_callback(cb["EVENT_LEAVE"], "OnQuit")
+    register_callback(cb["EVENT_GAME_END"], "OnEnd")
+    register_callback(cb["EVENT_CHAT"], "OnPlayerVote")
+    register_callback(cb["EVENT_GAME_START"], "OnStart")
+    OnStart()
+end
+
+function OnScriptUnload() end
