d7265a170d91be1a1a27014e1a0c829a2ec53889
Chalwk77 <jericho.crosby227@gmail.com>
2025-08-28 15:46:42 +1200

    Tweak to config variables, per naming conventions.

--- sapp/attractive/ammo_resupply.lua
+++ sapp/attractive/ammo_resupply.lua
@@ -9,47 +9,47 @@ DESCRIPTION:      Adds a configurable '/res' command that allows players to:
 
                   Server operators can configure all aspects of the resupply.
 
-Copyright (c) 2019-2024 Jericho Crosby
+Copyright (c) 2019-2025 Jericho Crosby
 LICENSE:          MIT License
                   https://github.com/Chalwk/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 =====================================================================================
 ]]
 -- Configuration table for the resupply script
-local config = {
+local CONFIG = {
 
     -- Command to trigger the resupply
-    command = 'res',
+    COMMAND = 'res',
 
     -- Permission level required to use the command (-1 means no restriction)
     -- Default: -1
-    permission_level = -1,
+    PERMISSION_LEVEL = -1,
 
     -- Amount of ammo to resupply
     -- Default: 200
-    ammo = 200,
+    AMMO = 200,
 
     -- Amount of magazine to resupply
     -- Default: 500
-    mag = 500,
+    MAG = 500,
 
     -- Number of frag grenades to resupply
     -- Default: 4
-    frags = 4,
+    FRAGS = 4,
 
     -- Number of plasma grenades to resupply
     -- Default: 4
-    plasmas = 4,
+    PLASMAS = 4,
 
     -- Battery percentage to resupply
     -- Default: 100%
-    battery = 100,
+    BATTERY = 100,
 
     -- Output message to display after resupply
-    output = '[RESUPPLY] +200 ammo, +500 mag, +4 frags, +4 plasmas, +100% battery',
+    OUTPUT = '[RESUPPLY] +200 ammo, +500 mag, +4 frags, +4 plasmas, +100% battery',
 
     -- Cooldown period in seconds before the command can be used again
     -- Default: 30 seconds
-    cooldown = 30
+    COOLDOWN = 30
 }
 
 -- SAPP Lua API Version
@@ -59,38 +59,38 @@ api_version = '1.12.0.0'
 local last_resupply = {}
 
 -- Function to check if a player has the required permission level
-local function hasPermission(player_id)
-    local level = tonumber(get_var(player_id, '$lvl'))
-    return level >= config.permission_level
+local function hasPermission(playerId)
+    local level = tonumber(get_var(playerId, '$lvl'))
+    return level >= CONFIG.PERMISSION_LEVEL
 end
 
 -- Function to handle the resupply command
-local function handleResupplyCommand(player_id)
+local function handleResupplyCommand(playerId)
     local now = os.time()
 
-    if config.cooldown > 0 and last_resupply[player_id] and now < last_resupply[player_id] + config.cooldown then
-        local wait_time = math.floor(last_resupply[player_id] + config.cooldown - now)
-        rprint(player_id, 'You must wait ' .. wait_time .. ' more seconds before resupplying.')
+    if CONFIG.COOLDOWN > 0 and last_resupply[playerId] and now < last_resupply[playerId] + CONFIG.COOLDOWN then
+        local wait_time = math.floor(last_resupply[playerId] + CONFIG.COOLDOWN - now)
+        rprint(playerId, 'You must wait ' .. wait_time .. ' more seconds before resupplying.')
         return
     end
 
-    last_resupply[player_id] = now
+    last_resupply[playerId] = now
 
     -- Update ammo
-    execute_command('ammo ' .. player_id .. ' ' .. config.ammo .. ' 5')
-    execute_command('mag ' .. player_id .. ' ' .. config.mag .. ' 5')
-    execute_command('battery ' .. player_id .. ' ' .. config.battery .. ' 5')
+    execute_command('ammo ' .. playerId .. ' ' .. CONFIG.AMMO .. ' 5')
+    execute_command('mag ' .. playerId .. ' ' .. CONFIG.MAG .. ' 5')
+    execute_command('battery ' .. playerId .. ' ' .. CONFIG.BATTERY .. ' 5')
 
     -- Update grenades
-    execute_command('nades ' .. player_id .. ' ' .. config.frags .. ' 1')
-    execute_command('nades ' .. player_id .. ' ' .. config.plasmas .. ' 2')
+    execute_command('nades ' .. playerId .. ' ' .. CONFIG.FRAGS .. ' 1')
+    execute_command('nades ' .. playerId .. ' ' .. CONFIG.PLASMAS .. ' 2')
 
-    rprint(player_id, config.output)
+    rprint(playerId, CONFIG.OUTPUT)
 end
 
 -- Event handler for command execution
 function OnCommand(player_id, command)
-    if command:sub(1, #config.command):lower() == config.command then
+    if command:sub(1, #CONFIG.COMMAND):lower() == CONFIG.COMMAND then
         if hasPermission(player_id) then
             handleResupplyCommand(player_id)
         else
@@ -100,12 +100,8 @@ function OnCommand(player_id, command)
     end
 end
 
--- Event handler for script load
 function OnScriptLoad()
     register_callback(cb['EVENT_COMMAND'], 'OnCommand')
 end
 
--- Event handler for script unload
-function OnScriptUnload()
-    -- No cleanup required
-end
+function OnScriptUnload() end
